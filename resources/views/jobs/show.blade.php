26f0bdd927e36ef1d9242602ec449de8432719a4
hamed ajaj <hamedsgaming@gmail.com>
2025-09-13 20:45:27 +0300

    learn about delete and update and blade methods directive

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -3,6 +3,10 @@
     <x-slot:heading>
         job Page
     </x-slot:heading>
-<h1>job {{$job['title']}}</h1>
-    pays ${{$job['salary']}} per year.
+    <h1>job {{$job->title}}</h1>
+    <p>pays ${{$job->salary}} per year.</p>
+    <p class="mt-6">
+    <x-button href="/jobs/{{$job->id}}/edit">Edit Job</x-button>
+    </p>
+
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,38 @@ Route::get('/jobs/{id}', function (string $id) {
         'job' => Job::find($id) ]);
 });
 
+Route::get('/jobs/{id}/edit', function (string $id) {
+
+    return view('jobs.edit',[
+        'job' => Job::find($id) ]);
+});
+
+Route::patch('/jobs/{id}', function ($id) {
+
+    request()->validate([
+        'title' => ['required', 'min:3', 'max:255'],
+        'salary' => ['required']
+    ]);
+
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    return redirect('/jobs/' .$job->id);
+});
+
+Route::delete('/jobs/{id}', function ($id) {
+
+    $job = Job::findOrFail($id);
+
+    $job->delete();
+
+    return redirect('/jobs');
+});
+
 Route::get('/contact', function () {
     return view('contact');
 });
